f7f510e4451a017d01d9a0b96f4b5a27128e8e0b
MatisYahoo <zzhozann@gmail.com>
2021-01-03 19:48:56 +0100

    First Client GUI concept

---
+++ SKGomokuClient/src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GUI GUI = new GUI();
+        GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GUI.setVisible(true);
+    }
+}
